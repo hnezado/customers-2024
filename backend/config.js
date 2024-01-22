cf7f4e3506f37a94c9af0b26592af3c3b4b21857
hnezado <hnezado@gmail.com>
2024-01-22 15:15:42 +0100

    Setup DB connection

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: {
+        host: '192.168.1.2',
+        user: 'hnezado',
+        password: 'mariadbpw',
+        database: 'hdatabase'
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const mysql = require('mysql2')
 const cors = require('cors')
+const config = require('./config.js')
 
 const app = express()
 const port = 8084
 
 app.use(cors())
 
+const conn = mysql.createConnection(config.db)
+
+conn.connect(error => {
+    if (error) {
+        console.error('Error connecting to db:', error)
+    } else {
+        console.log('Connection to db successful')
+    }
+})
+
 app.get('/', (req, res) => {
-    res.send('Esto es un mensaje desde el backend')
+    conn.query('SELECT * FROM customer', (error, results) => {
+        if (error) {
+            console.error('Error on the query:', error)
+            res.status(500).send('Server error')
+        } else {
+            res.json(results)
+        }
+    })
+    // res.send('Esto es un mensaje desde el backend')
 })
 
 app.listen(port, () => {
