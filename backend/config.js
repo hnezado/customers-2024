38a332f92315a37c6220dc2278a35e23d732e67c
hnezado <hnezado@gmail.com>
2024-01-22 15:29:27 +0100

    Transfering credentials to dotenv

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,12 @@
+const env = require('dotenv')
+
+env.config()
+
 module.exports = {
     db: {
-        host: '192.168.1.2',
-        user: 'hnezado',
-        password: 'mariadbpw',
-        database: 'hdatabase'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 }
