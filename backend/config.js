48645eef0211ff75cfdaa3b315f843305d32716c
hnezado <hnezado@gmail.com>
2024-02-04 09:17:45 +0100

    Login data retrieval functional

--- backend/config.js
+++ backend/config.js
@@ -10,4 +10,10 @@ module.exports = {
     database: process.env.DB_DATABASE,
     connectionLimit: 5,
   },
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204
+  }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,64 +1,336 @@
 const express = require("express");
-const db = require("mariadb");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const db = require("mariadb");
 const config = require("./config.js");
 
 const app = express();
 const port = 8084;
 
-app.use(cors());
+// Parses the body as json
+app.use(express.json());
 
+// Allows certain requests from origin depending on configuration
+// app.use(cors(config.cors));
+app.use(
+  cors({
+    origin: "http://192.168.1.10:8080",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
+
+// Creates a database connection pool
 const pool = db.createPool(config.db);
 
-app.get("/", async (req, res) => {
-  let conn;
+async function getPoolConn() {
+  // Opens the connection with the database
   try {
-    conn = await pool.getConnection();
-    console.log("conn:", conn);
-    const rows = await conn.query(
-      "SELECT * FROM customer WHERE is_deleted = 0",
+    return pool.getConnection();
+  } catch (err) {
+    const msg = "Pool connection error";
+    console.error(msg, err);
+    return res.status(500).json({ message: msg, error: err.message });
+  }
+}
+
+async function retrieveUserData(conn, key, value) {
+  try {
+    const userData = await conn.query(
+      `SELECT * FROM users WHERE ${key} = '${value}'`
     );
-    res.json(rows);
-  } catch (error) {
-    console.error("Query error:", error);
-    res.status(500).send("Database connection error");
+    return {
+      status: 200,
+      json: {
+        message: `User retrieved successfully`,
+        data: userData,
+      },
+    };
+  } catch (err) {
+    const msg = "Error retrieving data";
+    console.error(msg, err.message);
+    return {
+      status: 500,
+      json: {
+        message: msg,
+        error: err.message,
+      },
+    };
+  }
+}
+
+async function validateSignup(conn, signupData) {
+  // Validates data from user before registering it
+  try {
+    const existingEmail = await retrieveUserData(
+      conn,
+      "email",
+      signupData.email
+    );
+    if (existingEmail.json.data.length) {
+      return {
+        status: 400,
+        json: {
+          message: `This email is already registered`,
+          data: signupData,
+        },
+      };
+    }
+  } catch (err) {
+    const msg = "Error validating existing email";
+    console.error(msg, err.message);
+    return {
+      status: 500,
+      json: {
+        message: msg,
+        error: err.message,
+      },
+    };
+  }
+  try {
+    const existingUsername = await retrieveUserData(
+      conn,
+      "username",
+      signupData.username
+    );
+    if (existingUsername.json.data.length) {
+      return {
+        status: 400,
+        json: {
+          message: `This username is already registered`,
+          data: signupData,
+        },
+      };
+    }
+  } catch (err) {
+    const msg = "Error validating existing username";
+    console.error(msg, err.message);
+    return {
+      status: 500,
+      json: {
+        message: msg,
+        error: err.message,
+      },
+    };
+  }
+
+  // Password validation
+  try {
+    if (signupData.password.length < 8) {
+      return {
+        status: 400,
+        json: {
+          message: `The password must contain at least 8 characters`,
+          data: signupData,
+        },
+      };
+    }
+    const containsDigits = /\d/.test(signupData.password);
+    const containsLetters = /[a-zA-Z]/.test(signupData.password);
+    if (containsDigits && containsLetters) {
+      return {
+        status: 200,
+      };
+    } else {
+      return {
+        status: 400,
+        json: {
+          message: `The password must contain at least 1 digit and 1 letter`,
+          data: signupData,
+        },
+      };
+    }
+  } catch (err) {
+    const msg = "Error validating password";
+    console.error(msg, err.message);
+    return {
+      status: 500,
+      json: {
+        message: msg,
+        error: err.message,
+      },
+    };
+  }
+}
+
+async function validateLogin(loginData, userData) {
+  // Validates data from user to start its session
+  try {
+    if (!userData.length) {
+      return {
+        status: 400,
+        json: {
+          message: `This username is not registered`,
+          data: loginData,
+        },
+      };
+    }
+    const passwordMatch = await bcrypt.compare(
+      loginData.password,
+      userData[0].password
+    );
+    if (passwordMatch) {
+      return {
+        status: 200,
+        json: {
+          message: `Logged in successfully`,
+          data: userData[0],
+        },
+      };
+    } else {
+      return {
+        status: 400,
+        json: {
+          message: `The password does not match`,
+          data: loginData,
+        },
+      };
+    }
+  } catch (err) {
+    const msg = "Error validating login data";
+    console.error(msg, err.message);
+    return {
+      status: 500,
+      json: {
+        message: msg,
+        error: err.message,
+      },
+    };
+  }
+}
+
+async function registerUser(conn, userData) {
+  // Adds the user data into the database
+  try {
+    await conn.execute(
+      "INSERT INTO users (email, username, password) VALUES (?, ?, ?)",
+      [userData.email, userData.username, userData.password]
+    );
+    return {
+      status: 200,
+      json: {
+        message: `User registered successfully`,
+        // Sent username to auto show it on the login input
+        data: {
+          username: userData.username,
+        },
+      },
+    };
+  } catch (err) {
+    const msg = "Error inserting data in database";
+    console.error(msg, err.message);
+    return {
+      status: 500,
+      json: {
+        message: msg,
+        error: err.message,
+      },
+    };
+  }
+}
+
+app.post("/signup", async (req, res) => {
+  // Register a new user
+  let conn, result;
+  const { email, username, password } = req.body;
+  const signupData = { email: email, username: username, password: password };
+  try {
+    conn = await getPoolConn();
+    const validSignup = await validateSignup(conn, signupData);
+    if (validSignup.status === 200) {
+      const hashedPw = await bcrypt.hash(password, 10);
+      const newUser = {
+        email,
+        username,
+        password: hashedPw,
+      };
+      result = await registerUser(conn, newUser);
+      res.status(result.status).json(result.json);
+    } else {
+      res.status(validSignup.status).json(validSignup.json);
+    }
   } finally {
     if (conn) conn.release();
   }
 });
 
-// Si pongo aquí un patch me falla saveEdit y delete y no se por qué:
-app.put("/customers/edit/:id", async (req, res) => {
-	console.log("params:", req.params)
-  const customerId = req.params.id;
-	const updatedData = req.body;
-  console.log(`Editing customer with ID: ${customerId}`);
+app.post("/login", async (req, res) => {
+  // Logs in into the user session
+  let conn;
+  const { username, password } = req.body;
+  const loginData = { username: username, password: password };
+  try {
+    conn = await getPoolConn();
+    const userData = await retrieveUserData(
+      conn,
+      "username",
+      loginData.username
+    );
+    const validLogin = await validateLogin(loginData, userData.json.data);
+    return res.status(validLogin.status).json(validLogin.json);
+  } finally {
+    if (conn) conn.release();
+  }
+});
+
+app.get("/customers/list", async (req, res) => {
+  // Retrieves the customers list
   let conn;
   try {
     conn = await pool.getConnection();
-    await conn.query("UPDATE customer SET is_deleted = 1 WHERE id = ?", [
-      customerId,
-    ]);
-    res.status(204).end();
-  } catch (error) {
-    console.error("Edit error:", error);
+    const queryData = await conn.query(
+      "SELECT * FROM customers WHERE is_deleted = 0"
+    );
+    return res.status(200).json(queryData);
+  } catch (err) {
+    const msg = "Error querying the database";
+    console.error(msg, err.message);
+    return res.status(500).json({ message: msg, error: err.message });
+  } finally {
+    if (conn) conn.release();
+  }
+});
+
+// *Si pongo aquí un patch en vez de put me falla saveEdit y delete y no se por qué
+app.put("/customers/edit/:id", async (req, res) => {
+  // Updates the customer data
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    await conn.query(
+      "UPDATE customers SET firstname = ?, lastname = ?, email = ?, birthdate = ?, phone = ? WHERE id = ?",
+      [
+        req.body.firstname,
+        req.body.lastname,
+        req.body.email,
+        req.body.birthdate,
+        req.body.phone,
+        req.params.id,
+      ]
+    );
+    return res.status(204).end();
+  } catch (err) {
+    const msg = "Error updating the database";
+    console.error(msg, err.message);
+    return res.status(500).json({ message: msg, error: err.message });
   } finally {
     if (conn) conn.release();
   }
 });
 
 app.patch("/customers/delete/:id", async (req, res) => {
+  // Deletes the customer
   const customerId = req.params.id;
-  console.log(`Deleting customer with ID: ${customerId}`);
   let conn;
   try {
     conn = await pool.getConnection();
-    await conn.query("UPDATE customer SET is_deleted = 1 WHERE id = ?", [
+    await conn.query("UPDATE customers SET is_deleted = 1 WHERE id = ?", [
       customerId,
     ]);
-    res.status(204).end();
-  } catch (error) {
-    console.error("Delete error:", error);
+    return res.status(204).end();
+  } catch (err) {
+    console.error("Delete error:", err.message);
   } finally {
     if (conn) conn.release();
   }
